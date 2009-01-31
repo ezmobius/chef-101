3f7a5899c4470fab881d703420e23d2bda2086e8
Ezra Zygmuntowicz <ez@engineyard.com>
2009-01-31 10:22:14 -0800

    updating with demo recipes for getting started

--- cookbooks/gems/recipes/default.rb
+++ cookbooks/gems/recipes/default.rb
@@ -1,5 +1,11 @@
-
-
-gem_package "merb" do
-  version '1.0.8.1'
-end  
+node[:gems].each do |gem|
+  gem_package gem[:name] do
+    if gem[:version] && !gem[:version].empty?
+      version gem[:version]
+    end
+    if gem[:source]
+      source gem[:source]
+    end
+    action :install
+  end
+end
